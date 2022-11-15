999b9fd10e3fbf73275e2b46ff3c636b3014fe86
rakeshsai03 <rakeshsai03@gmail.com>
2022-11-15 14:40:01 +0530

    FirstCommit

--- GitHubIntegration/src/PrintHello.java
+++ GitHubIntegration/src/PrintHello.java
@@ -1,9 +1,9 @@
 
-public class Paint {
+public class PrintHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello!");
+		System.out.println("Hello World!");
 
 	}
 
